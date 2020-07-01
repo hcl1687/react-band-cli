6dbd6f8102429e3a7d957ad79e189dbe48951f93
Chunlin He <hechunlin@nd.com.cn>
2020-07-01 19:28:45 +0800

    init with demo

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 
 var fs = require('fs')
 var path = require('path')
+var ncp = require('ncp')
 const { program } = require('commander')
 const mkdirp = require('mkdirp')
 const download = require('download-git-repo')
@@ -11,14 +12,16 @@ const currentDir = process.cwd()
 program
   .usage('[options] <file ...>')
   .option('-i --init <name>', 'init project')
+  .option('-d --demo <name>', 'with demo')
   .parse(process.argv);
 
 if (program.init) {
-  initProject(program.init)
+  console.log()
+  initProject(program.init, program.demo)
 }
 
 // create project
-function initProject(projectName) {
+function initProject(projectName, demoName) {
   const projectPath = path.resolve(currentDir, projectName)
   console.log(projectPath)
   if (fs.existsSync(projectPath)) {
@@ -26,7 +29,27 @@ function initProject(projectName) {
   } else {
     fs.mkdirSync(projectPath)
     download(rbRepo, projectPath, (err) => {
-      console.log(err && err.message)
+      if (err) {
+        console.warn(err)
+        return
+      }
+
+      const demoPath = path.resolve(projectPath, 'demo/' + demoName)
+      console.log(demoPath)
+      if (!fs.existsSync(demoPath)) {
+        console.warn(demoName + ' not exists.')
+        return
+      }
+      copyDemo(projectPath, demoPath)
     })
   }
+}
+
+function copyDemo(dest, source) {
+  ncp(source, dest, (err) => {
+    if (err) {
+      console.warn(err)
+      return
+    }
+  })
 }
